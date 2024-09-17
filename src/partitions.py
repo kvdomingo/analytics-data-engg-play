1b7fdf1d6e254311b1c062062739a2c926644934
Kenneth V. Domingo <hello@kvd.studio>
2024-09-17 21:36:07 +0800

    feat: add countries as partitions

---
+++ src/partitions.py
@@ -0,0 +1,6 @@
+from country_converter import CountryConverter
+from dagster import StaticPartitionsDefinition
+
+countries_partitions_def = StaticPartitionsDefinition(
+    CountryConverter().data["ISO3"].tolist()
+)
