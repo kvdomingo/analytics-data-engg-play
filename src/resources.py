7ee369bcd1a803f15b86f6a2556b2398bf9adea9
Kenneth V. Domingo <hello@kvd.studio>
2024-09-25 20:58:41 +0800

    feat: add airbyte

--- src/resources.py
+++ src/resources.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from dagster import EnvVar
+from dagster_airbyte import AirbyteResource
 from dagster_dbt import DbtCliResource
 from dagster_duckdb import DuckDBResource
 from dagster_duckdb_polars import DuckDBPolarsIOManager
@@ -14,10 +16,17 @@ class IOManager(Enum):
 class Resource(Enum):
     DUCKDB = "duckdb"
     DBT = "dbt"
+    AIRBYTE = "airbyte"
 
 
 RESOURCES = {
     Resource.DUCKDB.value: DuckDBResource(database=settings.DUCKDB_DATABASE),
     Resource.DBT.value: DbtCliResource(project_dir=settings.BASE_DIR),
+    Resource.AIRBYTE.value: AirbyteResource(
+        host=settings.AIRBYTE_HOST,
+        port=str(settings.AIRBYTE_PORT),
+        username=EnvVar("AIRBYTE_USERNAME"),
+        password=EnvVar("AIRBYTE_PASSWORD"),
+    ),
     IOManager.DUCKDB.value: DuckDBPolarsIOManager(database=settings.DUCKDB_DATABASE),
 }
