8bea7c47b0c3cbb0f8d62b77127eb2af88ca352d
Kenneth V. Domingo <hello@kvd.studio>
2025-04-22 22:11:33 +0800

    feat: add disease_pidsr_location

--- src/assets/cchain/assets.py
+++ src/assets/cchain/assets.py
@@ -32,3 +32,32 @@ def cchain__climate_atmosphere_location(
         ).pl()["count"][0]
 
     context.add_output_metadata(emit_standard_df_metadata(df, row_count=count))
+
+
+@dg.asset(
+    group_name="cchain",
+    kinds={"duckdb"},
+    deps={"cchain__disease_pidsr_totals", "cchain__location"},
+)
+def cchain__disease_pidsr_location(
+    context: dg.AssetExecutionContext,
+    duckdb: DuckDBResource,
+):
+    conn: DuckDBPyConnection
+    with duckdb.get_connection() as conn:
+        conn.sql("""
+        CREATE OR REPLACE VIEW public.cchain__disease_pidsr_location AS (
+            SELECT *
+            FROM public.cchain__disease_pidsr_totals dpt
+            LEFT JOIN public.cchain__location l USING (adm3_pcode)
+            ORDER BY date
+        );
+        """)
+        df = conn.sql(
+            "SELECT * FROM public.cchain__disease_pidsr_location LIMIT 10"
+        ).pl()
+        count = conn.sql(
+            "SELECT COUNT(*) AS count FROM public.cchain__disease_pidsr_location"
+        ).pl()["count"][0]
+
+    context.add_output_metadata(emit_standard_df_metadata(df, row_count=count))
