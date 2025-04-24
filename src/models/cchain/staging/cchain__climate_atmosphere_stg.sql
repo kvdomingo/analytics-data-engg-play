13d82bb8d61551422722610b15824d5f554fb53f
Kenneth V. Domingo <hello@kvd.studio>
2025-04-25 00:01:40 +0800

    fix: coalesce null `pr` values to 0

--- src/models/cchain/staging/cchain__climate_atmosphere_stg.sql
+++ src/models/cchain/staging/cchain__climate_atmosphere_stg.sql
@@ -7,9 +7,9 @@ SELECT
     tmin::FLOAT AS tmin,
     tmax::FLOAT AS tmax,
     heat_index::FLOAT AS heat_index,
-    pr::FLOAT AS pr,
     wind_speed::FLOAT AS wind_speed,
     rh::FLOAT AS rh,
     solar_rad::FLOAT AS solar_rad,
-    uv_rad::FLOAT AS uv_rad
+    uv_rad::FLOAT AS uv_rad,
+    COALESCE(pr::FLOAT, 0.0) AS pr
 FROM {{ source('ae_de_play', 'cchain__climate_atmosphere_raw') }}
